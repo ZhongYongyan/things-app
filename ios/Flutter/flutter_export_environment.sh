43434bbd2bebf0da69b90d073ca08612f378c3b1
zhongren <zhrren@qq.com>
2019-11-04 11:03:16 +0800

    fixbug

--- ios/Flutter/flutter_export_environment.sh
+++ ios/Flutter/flutter_export_environment.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 # This is a generated file; do not edit or check into version control.
 export "FLUTTER_ROOT=/Applications/flutter"
-export "FLUTTER_APPLICATION_PATH=/Users/ren/Workspace/Team/engame/engame-app"
-export "FLUTTER_TARGET=/Users/ren/Workspace/Team/engame/engame-app/lib/main.dart"
+export "FLUTTER_APPLICATION_PATH=/Users/ren/Workspace/Shifen/things/things-app"
+export "FLUTTER_TARGET=/Users/ren/Workspace/Shifen/things/things-app/lib/main.dart"
 export "FLUTTER_BUILD_DIR=build"
 export "SYMROOT=${SOURCE_ROOT}/../build/ios"
 export "FLUTTER_FRAMEWORK_DIR=/Applications/flutter/bin/cache/artifacts/engine/ios"
