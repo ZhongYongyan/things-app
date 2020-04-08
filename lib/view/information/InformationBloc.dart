527ba68c690afbb47e806eada7b1488c26e8bbbf
quzhilin <914692514@qq.com>
2020-04-08 22:19:00 +0800

    fix

--- lib/view/information/InformationBloc.dart
+++ lib/view/information/InformationBloc.dart
@@ -59,6 +59,13 @@ class InformationBloc extends BlocBase with LoggingMixin {
     Result<Page> response =
         await InfoSortApis.getInfo(indexPage, 10, "ASC", sortId);
     bool code = response.success;
+    if(code) {
+      log.info("资讯请求出错",response.message);
+      setModel(() {
+        indexshow = false;
+      });
+      return;
+    }
     //错误处理
     lists = response.data.items;
     Future.delayed(Duration(seconds: 1)).then((e) {
