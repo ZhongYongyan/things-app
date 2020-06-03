9cf98cbe0b0980bd0c03637a46ecd84eba51e9d5
quzhilin <914692514@qq.com>
2020-06-03 23:38:20 +0800

    修改

--- lib/view/home/user/details/DatailsBloc.dart
+++ lib/view/home/user/details/DatailsBloc.dart
@@ -42,62 +42,55 @@ class DatailsBloc extends BlocBase with LoggingMixin {
   bool loginProcessing = false;
   bool addAffiliateShow = false;
   File images;
-  var heightPickerData = '''
-[
-    [
-        110,
-        120,
-        130,
-        140,
-        150,
-        160,
-        170,
-        180,
-        190,
-        200,
-        210,
-        220,
-        230,
-        240,
-        250
-    ]
-]
-    ''';
-  var weightPickerData = '''
-[
-    [
-        20,
-        30,
-        40,
-        50,
-        60,
-        70,
-        80,
-        90,
-        100,
-        110,
-        120,
-        130,
-        140,
-        150,
-        160,
-        170,
-        180,
-        190,
-        200,
-        210,
-        220,
-        230,
-        240,
-        250,
-        260,
-        270,
-        280,
-        290,
-        300
-    ]
-]
-    ''';
+  var heightPickerData =  [
+    "110",
+    "120",
+    "130",
+    "140",
+    "150",
+    "160",
+    "170",
+    "180",
+    "190",
+    "200",
+    "210",
+    "220",
+    "230",
+    "240",
+    "250"
+  ];
+  var weightPickerData =  [
+    "20",
+    "30",
+    "40",
+    "50",
+    "60",
+    "70",
+    "80",
+    "90",
+    "100",
+    "110",
+    "120",
+    "130",
+    "140",
+    "150",
+    "160",
+    "170",
+    "180",
+    "190",
+    "200",
+    "210",
+    "220",
+    "230",
+    "240",
+    "250",
+    "260",
+    "270",
+    "280",
+    "290",
+    "300"
+    ];
+
 
   void startup() {
     //retrieveData();
@@ -122,15 +115,15 @@ class DatailsBloc extends BlocBase with LoggingMixin {
     });
   }
 
-  void userClickHeight(List i) {
+  void userClickHeight(String i) {
     setModel(() {
-      affiliateModel.height = int.parse(i[0]);
+      affiliateModel.height = int.parse(i);
     });
   }
 
-  void weightClickHeight(List i) {
+  void weightClickHeight(String i) {
     setModel(() {
-      affiliateModel.weight = double.parse(i[0]);
+      affiliateModel.weight = double.parse(i);
     });
   }
 
